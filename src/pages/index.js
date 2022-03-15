0c254ae592280b3ea93784b9f3c27449e26dd208
Arthur Sousa <macbookpro16@Arthurs-MacBook-Pro.local>
2022-03-14 22:47:52 -0400

    translated video btn

--- src/pages/index.js
+++ src/pages/index.js
@@ -176,7 +176,7 @@ const IndexPage = (props) => {
                   <polygon className="play-btn__svg" points="9.33 6.69 9.33 19.39 19.3 13.04 9.33 6.69"/>
                   <path className ="play-btn__svg" d="M26,13A13,13,0,1,1,13,0,13,13,0,0,1,26,13ZM13,2.18A10.89,10.89,0,1,0,23.84,13.06,10.89,10.89,0,0,0,13,2.18Z"/>
                 </svg> 
-                Play Video
+                <FormattedMessage id='video-btn' />
               </Button>
 
               <Modal 
