232037f6375a82e96581ae4b40e7d4330e88d8ee
Arthur Sousa <macbookpro16@Arthurs-MacBook-Pro.local>
2022-03-20 14:03:40 -0400

    removed second parallax section

--- src/components/LoadableParallax.js
+++ src/components/LoadableParallax.js
@@ -3,7 +3,7 @@ import Base from '../images/base.png';
 import PostBgTwo from '../images/postsBgTwo.png';
 import {Flex, Image, Text} from '@chakra-ui/react';
 import {FormattedMessage} from 'react-intl';
-import {Parallax} from 'react-skrollr';
+import {Parallax, ParallaxProvider} from 'react-skrollr';
 
 const animateData = {
   'data-top-bottom': 'transform: translate(0px, -200px);',
@@ -12,64 +12,72 @@ const animateData = {
 
 const LoadableParallax = () => {
   return (
-    <Flex
-      overflow="hidden"
-      color="#000"
-      zIndex="3"
-      display="inline-flex"
-      alignItems={['center', 'center', 'flex-start', 'flex-start']}
-      justifyContent="center"
-      flexDirection="column"
-      bg="#e3711b"
-      bgImage={`url(${PostBgTwo})`}
-      bgRepeat="no-repeat"
-      bgSize={['420px', '420px', '650px', '650px']}
-      backgroundPosition={[
-        'left 5% top -10%',
-        'left 5% top -10%',
-        'left 0% top 0%',
-        'left 0% top 0%'
-      ]}
-      w="100vw"
-      minHeight="110vh"
-      pos="relative"
-      zindex="4"
+    <ParallaxProvider
+      init={{
+        smoothScrollingDuration: 100,
+        smoothScrolling: true,
+        forceHeight: false
+      }}
     >
-      <Parallax data={animateData}>
-        <Flex overflow="hidden" w={['100vw', '100vw', '65vw', '65vw']}>
-          <Image
-            src={Base}
-            title={"laurentian mlitary base in St-Adolphe-d'Howard, Quebec"}
-            w="100%"
-            objectFit="cover"
-          />
-        </Flex>
-      </Parallax>
-
       <Flex
-        flexDir="column"
-        w={['90vw', '90vw', '30vw', '30vw']}
-        pos={['relative', 'relative', 'absolute', 'absolute']}
-        right="2vw"
-        textAlign={['left', 'left', 'right', 'right']}
-        p={['20px', '20px', '0 5vw 0 0', '0 5vw 0 0']}
-        color="#fff"
+        overflow="hidden"
+        color="#000"
+        zIndex="3"
+        display="inline-flex"
+        alignItems={['center', 'center', 'flex-start', 'flex-start']}
+        justifyContent="center"
+        flexDirection="column"
+        bg="#e3711b"
+        bgImage={`url(${PostBgTwo})`}
+        bgRepeat="no-repeat"
+        bgSize={['420px', '420px', '650px', '650px']}
+        backgroundPosition={[
+          'left 5% top -10%',
+          'left 5% top -10%',
+          'left 0% top 0%',
+          'left 0% top 0%'
+        ]}
+        w="100vw"
+        minHeight="110vh"
+        pos="relative"
+        zindex="4"
       >
-        <Text
-          as="h1"
-          margin="0px"
-          zIndex="3"
-          fontSize={['10vw', '10vw', '3vw', '3vw']}
-          fontFamily="Helvetica"
-          fontWeight="bolder"
+        <Parallax data={animateData}>
+          <Flex overflow="hidden" w={['100vw', '100vw', '65vw', '65vw']}>
+            <Image
+              src={Base}
+              title={"laurentian mlitary base in St-Adolphe-d'Howard, Quebec"}
+              w="100%"
+              objectFit="cover"
+            />
+          </Flex>
+        </Parallax>
+
+        <Flex
+          flexDir="column"
+          w={['90vw', '90vw', '30vw', '30vw']}
+          pos={['relative', 'relative', 'absolute', 'absolute']}
+          right="2vw"
+          textAlign={['left', 'left', 'right', 'right']}
+          p={['20px', '20px', '0 5vw 0 0', '0 5vw 0 0']}
+          color="#fff"
         >
-          -Post-
-        </Text>
-        <Text as="p" zIndex="3" lineHeight="2rem">
-          <FormattedMessage id="laurentian-military" />
-        </Text>
+          <Text
+            as="h1"
+            margin="0px"
+            zIndex="3"
+            fontSize={['10vw', '10vw', '3vw', '3vw']}
+            fontFamily="Helvetica"
+            fontWeight="bolder"
+          >
+            -Post-
+          </Text>
+          <Text as="p" zIndex="3" lineHeight="2rem">
+            <FormattedMessage id="laurentian-military" />
+          </Text>
+        </Flex>
       </Flex>
-    </Flex>
+    </ParallaxProvider>
   );
 };
 
